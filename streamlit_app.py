422225935aaf6d31d107b3786958169c249cae79
Alan1657 <83606802+Alan1657@users.noreply.github.com>
2023-07-20 20:33:05 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,18 @@
 import streamlit as st
-import graphviz as graphviz
+from graphviz import Digraph
 
 def draw_context_diagram():
-    dot = graphviz.Digraph(format='png')
-    
-    dot.node('Sistema', shape='box', style='filled', fillcolor='lightblue', label='Lista de Requerimientos\nFuncionales y No Funcionales')
-    dot.node('Usuarios', shape='oval', style='filled', fillcolor='lightgreen', label='Usuarios\n(Padres, Docentes, Alumnos)')
-    dot.node('Cliente', shape='oval', style='filled', fillcolor='lightcoral', label='Cliente\n(Colegios peruanos\ny Equipo Directivo)')
-    dot.node('Administrador', shape='oval', style='filled', fillcolor='lightyellow', label='Administrador\ndel sistema')
+    dot = Digraph(comment='Context Diagram', format='png')
+    dot.node('Sistema', 'Sistema')
+    dot.node('Usuarios', 'Usuarios')
+    dot.node('Cliente', 'Cliente')
+    dot.node('Administrador', 'Administrador')
+    dot.edges([('Sistema', 'Usuarios'), ('Sistema', 'Cliente'), ('Sistema', 'Administrador')])
 
-    dot.edges(['Sistema -> Usuarios', 'Sistema -> Cliente', 'Sistema -> Administrador'])
-    
     st.graphviz_chart(dot.source)
 
 def main():
-    st.title('Diagrama de Contexto')
-    st.markdown('''
-    Este es un ejemplo de un diagrama de contexto creado con Streamlit y Graphviz.
-    ''')
+    st.title("Diagrama de Contexto")
     draw_context_diagram()
 
 if __name__ == "__main__":
